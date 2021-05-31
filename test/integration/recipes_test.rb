94b859012994699b08e9f4d91582c6e51e778b91
rtink2 <tinkham0408@gmail.com>
2021-05-31 10:43:24 -0400

    add create new recipe with tests

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -3,9 +3,9 @@ require "test_helper"
 class RecipesTest < ActionDispatch::IntegrationTest
 
   def setup
-    @chef = Chef.create!(chefname: "levi", email: "levi@example.com")
-    @recipe = Recipe.create(name: "italian pizza", description: "great pizza", chef: @chef)
-    @recipe2 = @chef.recipes.build(name: "breakfast burrito", description: "great breakfast")
+    @chef = Chef.create!(chefname: "Levi", email: "levi@example.com")
+    @recipe = Recipe.create(name: "Italian pizza", description: "Great pizza", chef: @chef)
+    @recipe2 = @chef.recipes.build(name: "Breakfast burrito", description: "Great breakfast")
     @recipe2.save
   end
 
@@ -17,8 +17,40 @@ class RecipesTest < ActionDispatch::IntegrationTest
   test "should get recipe listing" do
     get recipes_path
     assert_template 'recipes/index'
+    assert_select "a[href=?]", recipe_path(@recipe), text: @recipe.name
+    assert_select "a[href=?]", recipe_path(@recipe2), text: @recipe2.name
+  end
+
+  test "should get recipes show" do
+    get recipe_path(@recipe)
+    assert_template 'recipes/show'
     assert_match @recipe.name, response.body
-    assert_match @recipe2.name, response.body
+    assert_match @recipe.description, response.body
+    assert_match @chef.chefname, response.body
+  end
+
+  test "create new valid recipe" do
+    get new_recipe_path
+    assert_template 'recipes/new'
+    name_of_recipe = 'Italian pizza'
+    description_of_recipe = 'great pizza'
+    assert_difference 'Recipe.count', 1 do
+      post recipes_path, params: { recipe: { name: name_of_recipe, description: description_of_recipe } }
+    end
+    follow_redirect!
+    assert_match name_of_recipe.capitalize, response.body
+    assert_match description_of_recipe.capitalize, response.body
+  end
+
+  test "reject invalid recipe submissions" do
+    get new_recipe_path
+    assert_template 'recipes/new'
+    assert_no_difference 'Recipe.count' do
+      post recipes_path, params: { recipe: { name: " ", description: " " } }
+    end
+    assert_template 'recipes/new'
+    assert_select 'h2.error-title'
+    assert_select 'div.error-body'
   end
 
 end
