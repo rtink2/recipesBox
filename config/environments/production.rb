65496209ee00fce6a3f3be0d362feef62d13f5c9
rtink2 <tinkham0408@gmail.com>
2021-08-11 20:35:02 -0400

    make app production ready with actioncable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,6 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.web_socket_server_url = "wss://rhonda-recipebox.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = [ 'https://rhonda-recipebox.herokuapp.com', 'http://rhonda-recipebox.herokuapp.com' ]
 end
